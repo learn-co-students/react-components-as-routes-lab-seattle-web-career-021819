3c0ff074104289e447da73df5a580d4b9f6fc8e0
ytsuji27 <ytsuji91@gmail.com>
2019-04-30 15:41:36 -0700

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,25 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        <ul>
+          {movies.map((movie, index) => {
+            return (
+              <div key={index}>
+                <h3>Name: {movie.title}</h3>
+                Time: {movie.time}<br /><br />
+                Genres:
+                  <ul>
+                    {movie.genres.map((genre, index) => {
+                      return (
+                        <li key={index}>{genre}</li>
+                      )
+                    })}
+                  </ul>
+              </div>
+            )
+          })}
+        </ul>
     </div>
   );
 };
